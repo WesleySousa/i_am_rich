5c8d0470c6721691bb20dc181faaa2da92e9b2a0
Wesley Silva <wesley.sousa59@gmail.com>
2020-04-22 16:37:50 -0300

    Scaffolding a Material App

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
     MaterialApp(
-      home: Center(
-         child: Text("Hello World"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I\'m Rich'),
+          backgroundColor: Colors.blueGrey[900],
         ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
+      ),
     ),
 );
